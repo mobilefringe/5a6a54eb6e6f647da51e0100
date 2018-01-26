40820ee6470ef9aed32aa19cf5c51a07d6d2ab8e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-26 10:04:58 -0500

    Indigo Template 1.0  /main.js

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                     },
                     {
                         name: "Dining",
-                        href: "/events",
+                        href: "/dine",
                         sub_menu: [{
                                 name: "Leasing ",
                                 href: "/pages/eastgate-leasing"
