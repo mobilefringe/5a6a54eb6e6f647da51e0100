3f8b54327386655b9b03e4c8d8fdb1f478d1b6d5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-26 10:03:07 -0500

    Indigo Template 1.0  /main.js

--- main.js
+++ main.js
@@ -112,7 +112,6 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                     },
                     {
                         name: "Contact Us",
-                        id: "contact_link",
                         href: "",
                         show_sub_menu: false,
                         sub_menu: [
