583482cc2cf4da6154ab9136b2fb2718b3379841
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-29 10:19:50 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                         name: "Stores",
                         href: "/stores",
                         sub_menu: [{
-                                name: "Directory ",
+                                name: "Stores ",
                                 href: "/stores"
                             },
                             {
