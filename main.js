44b08cc718929b8bf50421af52843a1f0bc354c9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-29 10:21:09 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                             },
                             {
                                 name: "Community Services",
-                                href: "/pages/eastgate-speciality-leasing-inquiries"
+                                href: "/pages/community_services"
                             },
                             {
                                 name: "Photos",
