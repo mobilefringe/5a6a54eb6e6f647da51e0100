2b8c48087fd2f02f384adf3b7f1d1ff7f83cc818
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-29 10:20:31 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                     },
                     {
                         name: "Guest Services",
-                        href: "",
+                        href: "/services",
                         sub_menu: [{
                                 name: "Leasing ",
                                 href: "/pages/eastgate-leasing"
