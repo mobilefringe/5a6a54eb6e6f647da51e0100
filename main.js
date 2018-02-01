87cd610adf75995d5a66889f017e3bc7bc454776
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-01 13:34:49 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -146,7 +146,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                 suggestionAttribute: 'name',
                 newsletter_email: "",
                 stickyMenu: false,
-                isSticky: 200,
+                isSticky: 180,
             }
         },
         created() {
