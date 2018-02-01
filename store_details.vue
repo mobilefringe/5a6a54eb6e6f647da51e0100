7917b13fd27145ed5dea84b50d4dada081733370
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-01 11:53:28 -0500

    Bonnie Doon 1.0  /store_details.vue

---
+++ store_details.vue
@@ -0,0 +1,173 @@
+<template>
+	<div class="page_container store_dets_container" v-if="dataLoaded" id="store_dets_container">
+		<div class="row">
+			<div class="col-sm-4 store_logo_container">
+				<div>
+					<img :src="currentStore.store_front_url_abs"/>
+				</div>
+			</div>
+			<div class="col-sm-8 store_map_container">
+				<div id="mapsvg_store_detail">
+					<png-map v-bind:png-map-url="getPNGurl" v-bind:initial-position="'150 150'"></png-map>
+				</div>
+			</div>
+		</div>
+		<div class="row is-table-row">
+			<div class="col-sm-4 store_details_container">
+				<div>
+					<h1>{{currentStore.name}}</h1>
+					<p>{{currentStore.category_name}}</p>
+					<p>{{currentStore.phone}}</p>
+					<a v-bind:href="'//'+currentStore.website" target="_blank">Visit Store Site</a>
+				</div>
+			</div>
+			<div class="col-sm-8 store_desc_container">
+				<div class="text-left store_description">
+					<p>{{currentStore.description}}</p>
+				</div>
+			</div>
+		</div>
+		<div class="store_promo_container" v-if="currentStore && currentStore.total_published_promos > 0">
+		    <div class="promo_container_title text-left all_caps"> Sales & Promotions</div>
+		    <div class="row store_promo_dets text-left" v-for="promo in promotions">
+		        <div class="col-sm-7" >
+		        <div class="promo_div_image">
+		            <img :src="promo.image_url" alt=""/>
+		        </div>
+		        </div>
+		        <div class="col-sm-5 promo_div_dets">
+		            <p class="promo_div_name">{{promo.name}}</p>
+		            <p class="promo_div_store_name">{{currentStore.name | uppercase}}</p>
+		            <p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
+					<p class="promo_div_description">{{promo.description_short}}</p>
+					<span class="feature_read_more">
+						<router-link :to="'/promotions/'+promo.slug" class="mobile_readmore" >
+							<p class="feature-readmore">Read More <i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>
+						</router-link>
+					</span>
+		        </div>
+		    </div>
+		</div>
+		<div class="store_promo_container" v-if="currentStore && currentStore.total_published_jobs > 0">
+		    <div class="promo_container_title text-left all_caps"> Careers</div>
+		    <div class="row store_promo_dets text-left" v-for="promo in jobs">
+		        <div class="col-sm-7" >
+		        <div class="promo_div_image">
+		            <img :src="currentStore.store_front_url_abs" alt=""/>
+		        </div>
+		        </div>
+		        <div class="col-sm-5 promo_div_dets">
+		            <p class="promo_div_name">{{promo.name}}</p>
+		            <p class="promo_div_store_name">{{currentStore.name | uppercase}}</p>
+		            <p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
+		            <p class="promo_div_description">{{promo.description_short}}</p>
+					<span class="feature_read_more">
+						<router-link :to="'/promotions/'+promo.slug" class="mobile_readmore" >
+							<p class="feature-readmore">Read More <i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>
+						</router-link>
+					</span>
+		        </div>
+		    </div>
+		</div>
+	</div>
+</template>
+<script>
+    define(['Vue', 'vuex', 'moment', "jquery", "smooth-zoom", "vue!png-map"], function(Vue, Vuex, moment, $, smoothZoom, PNGMapComponent) {
+        return Vue.component("store-details-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    currentStore: null,
+                    promotions : [],
+                    jobs:[],
+                    dataLoaded: false
+                }
+            },
+            props:['id'],
+            beforeRouteUpdate(to, from, next) {
+                this.currentStore = this.findStoreBySlug(to.params.id);
+                if (this.currentStore === null || this.currentStore === undefined){
+                    this.$router.replace({ name: '404'});
+                }
+                next();
+            },
+            created (){
+                this.loadData().then(response => {
+                    this.dataLoaded = true;
+                    this.updateCurrentStore(this.id);
+                });
+            },
+            watch: {
+                currentStore: function() {
+                    if ( _.includes(this.currentStore.store_front_url_abs, 'missing')) {
+                        this.currentStore.store_front_url_abs = "//codecloud.cdn.speedyrails.net/sites/5a6a54eb6e6f647da51e0100/image/png/1516652189884/ES_logo_red2.png";
+                    }
+                    var vm = this;
+                    var temp_promo = [];
+                    var temp_job = [];
+                    _.forEach(this.currentStore.promotions, function(value, key) {
+                        var current_promo = vm.findPromoById(value);
+                        current_promo.description_short = _.truncate(current_promo.description, {
+                            'length': 70
+                        });
+                        temp_promo.push(current_promo);
+                    });
+                    _.forEach(this.currentStore.jobs, function(value, key) {
+                        var current_job = vm.findJobById(value);
+                        current_job.description_short = _.truncate(current_job.description, {
+                            'length': 70
+                        });
+                        temp_job.push(current_job);
+
+                    })
+                    this.promotions = temp_promo;
+                    this.jobs = temp_job;
+                    
+                    setTimeout(function() {
+                        vm.addLandmark(vm.currentStore);
+                    }, 500);
+                },
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'processedStores',
+                    'findStoreBySlug',
+                    'findPromoById',
+                    'findJobById',
+                ]),
+                getPNGurl () {
+                    return "https://www.mallmaverick.com" + this.property.map_url;
+                },
+                svgMapRef() {
+                    return _.filter(this.$children, function(o) {
+                        return (o.$el.className == "svg-map")
+                    })[0];
+                },
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "jobs")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                updateCurrentStore (id) {
+                    this.currentStore = this.findStoreBySlug(id);
+                    if (this.currentStore === null || this.currentStore === undefined){
+                        this.$router.replace({ name: '404'});
+                    }
+                },
+                updateSVGMap(map) {
+                    this.map = map;
+                },
+                addLandmark(store) {
+                    this.svgMapRef.addMarker(store);
+                },
+            }
+        });
+    });
+</script>
