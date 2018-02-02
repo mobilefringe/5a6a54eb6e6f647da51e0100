08cdb7ba0f1febc1a938e0de8e92f0add3aaff8d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-02 09:54:59 -0500

    Bonnie Doon 1.0  /page_details.vue

---
+++ page_details.vue
@@ -0,0 +1,78 @@
+<template>
+    <div id="pages_container" v-if="currentPage">
+        <div class="page_header all_caps double_border_bottom">
+			<div class="page_container text_left" v-html="currentPage.title"> </div>
+		</div>
+        <div class="page_container">
+            <div class="row padding_30" >
+                <div class="page_body description_text text_left" v-html="currentPage.body">
+                    
+                </div>
+            </div>
+        </div>
+        <div style="padding:20px 0;"></div>
+    </div>
+    <!--Pages Banner-->
+</template>
+<style>
+    .page_title {
+        /*border-top:1px solid #aea99e;*/
+        border-bottom:1px solid #aea99e;
+        height: 35px;
+        line-height: 35px;
+    }
+    #pages_container img{
+        width: 100%;
+        height: auto;
+    }
+</style>
+<script>
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
+        return Vue.component("page-details-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    success_subscribe: false,
+                    currentPage: null
+                }
+            },
+            props:['id'],
+            beforeRouteUpdate(to, from, next) {
+                this.$store.dispatch('LOAD_PAGE_DATA', {
+                    url: this.property.mm_host + "/pages/" + to.params.id + ".json"
+                }).then(response => {
+                    this.currentPage = response.data;
+                }, error => {
+                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                    this.$router.replace({
+                        name: '404'
+                    });
+                });
+                next();
+            },
+            created(){
+               this.updateCurrentPage(this.id);
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone'
+                ])
+            },
+            methods: {
+                updateCurrentPage (id) {
+                    this.$store.dispatch('LOAD_PAGE_DATA', {
+                        url: this.property.mm_host + "/pages/" + id + ".json"
+                    }).then(response => {
+                        this.currentPage = response.data;
+                    }, error => {
+                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                        this.$router.replace({
+                            name: '404'
+                        });
+                    });
+                },
+            }
+        });
+    });
+</script>
