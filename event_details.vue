9bd86029d8d1618ff9d459146aa8bfef419b2aed
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-01 19:35:33 -0500

    Bonnie Doon 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -29,7 +29,7 @@
 				<!--</div>-->
 			</div>
 			<div class="col-sm-8 promo_image_container text-left">
-			<router-link to="/event"><i class="fa fa-angle-left"></i> &nbsp; Back to Events</router-link>
+			<router-link to="/events"><i class="fa fa-angle-left"></i> &nbsp; Back to Events</router-link>
 			    <h3 class="promo_name" style="margin: 20px auto 0px;">{{currentEvent.name}}</h3>
 			    <div class="row">
 			        <p class="promo_div_date pull-left">{{currentEvent.start_date | moment("MMM D", timezone)}} - {{currentEvent.end_date | moment("MMM D", timezone)}}</p>
