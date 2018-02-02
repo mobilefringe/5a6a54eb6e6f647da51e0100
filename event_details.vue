850885fe0679083aae7885bdf1aa6d07658898e7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-01 19:31:54 -0500

    Bonnie Doon 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -28,7 +28,7 @@
                     </div>
 				</div>
 			</div>
-			<div class="col-sm-8 event_image_container text-left">
+			<div class="col-sm-8 promo_image_container text-left">
 			<router-link to="/event"><i class="fa fa-angle-left"></i> &nbsp; Back to Eventtions</router-link>
 			    <h3 class="event_name" style="margin: 20px auto 0px;">{{currentEvent.name}}</h3>
 			    <div class="row">
