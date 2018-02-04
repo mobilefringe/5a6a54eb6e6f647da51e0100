98399db4e0a5d2138695905767ba2431a96f7766
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-03 19:54:18 -0500

    Bonnie Doon 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -31,13 +31,14 @@
                                 <input v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="Email" data-vv-delay="500">
                                 <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
                             </div>
-                        </div>
-                        <div class="form-group">
                             <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
                                 <label class="label" for="message">Message</label>
                                 <textarea v-model="form_data.message" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="message" type="text" placeholder="Message" data-vv-delay="500"></textarea>
                                 <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
                             </div>
+                        </div>
+                        <div class="form-group">
+                            
                         </div>
                         <div class="form-group">
                             <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
