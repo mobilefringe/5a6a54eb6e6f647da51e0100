4c862d7b3b8e51e1c1d12ceec601fd71e67d8d87
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-03 19:38:29 -0500

    Bonnie Doon 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -131,7 +131,7 @@
                 this.form_data.name = null;
                 this.form_data.email = null;
                 this.form_data.phone = null;
-                this.form_data.subject = null;
+                this.form_data.subject = this.property.name + ' Contact Us Form';
                 this.form_data.message = null;
             },
             computed: {
