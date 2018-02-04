24a0a72bae7cd1164c4058a2ced63c29e91c2317
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-03 19:14:47 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                     },
                     {
                         name: "Contact Us",
-                        href: "/hours",
+                        href: "/contact_us",
                         sub_menu: [
                             {
                                 name: "Hours",
