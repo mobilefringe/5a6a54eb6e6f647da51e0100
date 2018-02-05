2dc821a6111fe76cdddb96063696b4e6aa4adad9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-04 20:34:48 -0500

    Bonnie Doon 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -62,7 +62,7 @@
             		            <img v-lazy="promo.image_url" class="image" alt=""/>
             		        </div>
             		        </div>
-            		        <div class="col-sm-6 promo_div_dets">
+            		        <div class="col-sm-5 promo_div_dets">
             		            <p class="promo_div_name">{{promo.name}}</p>
             		            <p class="promo_div_promo_name">{{promo.store.name | uppercase}}</p>
             		            <p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
