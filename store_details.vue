cab6d0a6098e9c802f3657b3a274e4d9170a236e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-04 20:35:25 -0500

    Bonnie Doon 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -56,17 +56,6 @@
 									<img v-lazy="promo.image_url" class="image" alt=""/>
 								</div>
 							</div>
-							<div class="col-sm-5 promo_div_dets">
-								<p class="promo_div_name">{{promo.name}}</p>
-								<p class="promo_div_promo_name">{{promo.store.name | uppercase}}</p>
-								<p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
-								<p class="promo_div_description">{{promo.description_short}}</p>
-								<span class="feature_read_more">
-									<router-link :to="'/promotions/'+promo.slug" class="mobile_readmore" >
-										<p class="feature-readmore">Read More <i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>
-									</router-link>
-								</span>
-							</div>
 						</div>
 					</div>
 				</div>
