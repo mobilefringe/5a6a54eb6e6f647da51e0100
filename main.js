37cf7f37ac9c56dd2f442bb52a5f6715d5fdab9c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-05 09:31:32 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -233,8 +233,6 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                 this.$router.push("/stores/"+option.slug);
             },
             enableStickyNavbar(e) {
-                // console.log("scrolling");
-                // console.log(window.pageYOffset,this.isSticky,window.pageYOffset >= this.isSticky );
                 if (window.pageYOffset >= this.isSticky) {
                     this.stickyMenu = true;
                 } else {
