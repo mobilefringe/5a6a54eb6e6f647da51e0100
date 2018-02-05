8a0426235a2ee208ea3edaa5c6adc79e4357b5a0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-04 22:26:28 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -153,6 +153,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
         created() {
             this.loadData().then(response => {
                 this.dataLoaded = true;
+                this.locale = "en";
             });
             window.addEventListener('scroll', this.enableStickyNavbar);
         },
