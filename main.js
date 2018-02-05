9681a04522cb050c4de9441c3554df413158c3f9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-05 12:23:22 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -129,7 +129,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                                 href: "/hours"
                             },
                             {
-                                name: "Jobs",
+                                name: "header.jobs",
                                 href: "/jobs"
                             },
                             {
