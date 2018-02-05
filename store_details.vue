c01d9388108f05ebd734b08fa0c592cd77af1079
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-04 19:57:38 -0500

    Bonnie Doon 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -24,7 +24,7 @@
     			    
     				<div class="col-sm-12 no_padding">
     				    <!--<img v-lazy="currentStore.image_url"/>-->
-        				<png-map v-bind:png-map-url="getPNGurl" v-bind:initial-position="'500 450'" height="360"></png-map>
+        				<png-map v-bind:png-map-url="getPNGurl" v-bind:initial-position="'500 450'" height="460"></png-map>
     				</div>
     				
     			</div>
