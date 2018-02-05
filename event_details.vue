13b0cc2713acba2dbad126087441f5f7b0efe270
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-05 15:35:11 -0500

    Bonnie Doon 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -32,7 +32,7 @@
 						<img v-lazy="currentEvent.image_url" class="image"/>
 						<div class="text-left promo_description">
 							<p v-if="locale=='en-ca'" v-html="currentEvent.rich_description"></p>
-							<p v-else v-html="currentEvent.rich_description"></p>
+							<p v-else v-html="currentEvent.rich_description_2"></p>
 						</div>
 					</div>
 				</div>
