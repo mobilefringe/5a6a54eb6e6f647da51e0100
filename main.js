1496ae804cac4298a2149f579d641834cc54481d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-04 20:05:24 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -211,7 +211,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
             loadData: async function() {
                 try {
                     await this.$store.dispatch('initializeApi', {
-                        site: "thegateway",
+                        site: "northpark",
                         version: "v4"
                     });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
