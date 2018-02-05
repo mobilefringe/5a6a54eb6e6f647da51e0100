9f927971d63f2b2aa33d043b971c726f65768d28
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-05 15:10:39 -0500

    Bonnie Doon 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -162,7 +162,9 @@
                         value.description_short = _.truncate(value.description, {
                             'length': 150
                         });
-                        
+                        value.description_short_2 = _.truncate(value.description_2, {
+                            'length': 150
+                        });
                         if (value.store != null && value.store != undefined && _.includes(value.store.image_url, 'missing')) {
                             value.store.image_url = "http://via.placeholder.com/400x400/757575";
                         }
