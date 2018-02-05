5d66c6d4bd78407f9ab4ae93d51a8f1a403b3ad0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-04 20:48:06 -0500

    Bonnie Doon 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -60,8 +60,8 @@
     								<p class="promo_div_date"><i class="fa fa-calendar-alt"></i>{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
     								<p class="promo_div_description">{{promo.description_short}}</p>
     								<span class="feature_read_more">
-    									<router-link :to="'/promotions/'+promo.slug" class="mobile_readmore" >
-    										<p class="contest_btn">Read More</p>
+    									<router-link :to="'/promotions/'+promo.slug" class="store_details_btn" >
+    										<p class="feature-readmore">Read More</p>
     									</router-link>
     								</span>
 								</div>
