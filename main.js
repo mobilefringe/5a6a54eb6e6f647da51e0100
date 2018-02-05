33fe5f136f25063f75fc9d9b2987680616aa11f3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-05 11:36:21 -0500

    Bonnie Doon 1.0  /main.js

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                 show_mobile_menu: false,
                 menu_items: [
                     {
-                        name: "Stores",
+                        name: this.$t("header.stores"),
                         href: "/stores",
                         sub_menu: [{
                                 name: "Stores ",
