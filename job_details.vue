b565476bbc9f85bab7a8b955fd81ec5830ce76a8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-13 16:09:56 -0500

    Bonnie Doon 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -19,7 +19,7 @@
 						    <h4 class="event_store_name caps" v-else>{{currentJob.store.name_2}}</h4>
 						</div>
 						<div v-else>
-						    <h4 class="event_store_name caps" v-else>{{currentJob.store.name_2}}</h4>
+						    <h4 class="event_store_name caps" v-if="property">{{currentJob.store.name_2}}</h4>
 						</div>
 						<h4 v-if="currentJob.store.phone" class="store_dets_title"> {{currentJob.store.phone}}</h4>
 						<h4 v-if="currentJob.store.website" class="store_dets_title"> <a :href="'//'+currentJob.store.website" target="_blank">{{$t("stores_page.store_website")}}</a></h4>
