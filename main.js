e8fd1e3f3ad73f9d8904d52f758ab3537a1e5728
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-05 14:51:19 -0500

    Bonnie Doon 1.1  /main.js

--- main.js
+++ main.js
@@ -92,7 +92,8 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                             },
                             {
                                 name: "header.download_directory",
-                                href: "/"
+                                href: "/",
+                                external: true
                             }
                         ]
                     },
