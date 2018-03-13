0458964d9490b494802164fae2c03ea6ea534e3e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-13 12:52:15 -0400

    Bonnie Doon 1.1  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -44,31 +44,11 @@
             },
             props:['id', 'locale'],
             beforeRouteUpdate(to, from, next) {
-                this.loadData(to.params.id).then(response => {
-                    if(response == null || response == undefined) {
-                        this.$router.replace('/');
-                    }
-                    this.currentPage = response[0].data;
-                    var temp_repo = this.findRepoByName('Pages Banner');
-                    if(temp_repo) {
-                        this.pageBanner = temp_repo.images[0];
-                    }
-                    this.pageBanner = this.pageBanner;
-                });
+                this.updatePageData(to.params.id);
                 next();
             },
             created(){
-                this.loadData(this.id).then(response => {
-                    if(response == null || response == undefined) {
-                        this.$router.replace('/');
-                    }
-                    this.currentPage = response[0].data;
-                    var temp_repo = this.findRepoByName('Pages Banner');
-                    if(temp_repo) {
-                        this.pageBanner = temp_repo.images[0];
-                    }
-                    this.pageBanner = this.pageBanner;
-                });
+               this.updatePageData(this.id);
             },
             computed: {
                 ...Vuex.mapGetters([
@@ -81,13 +61,26 @@
                 loadData: async function(id) {
                     try {
                         // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-                        // console.log("this.id", this.id);
+                        console.log("this.id", this.id);
                         let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/" + id + ".json"}),this.$store.dispatch("getData", "repos")]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
                 },
+                updatePageData (id) {
+                    this.loadData(id).then(response => {
+                        if(response == null || response == undefined) {
+                            this.$router.replace('/');
+                        }
+                        this.currentPage = response[0].data;
+                        var temp_repo = this.findRepoByName('Pages Banner');
+                        if(temp_repo) {
+                            this.pageBanner = temp_repo.images[0];
+                        }
+                        this.pageBanner = this.pageBanner;
+                    });
+                }
             }
         });
     });
