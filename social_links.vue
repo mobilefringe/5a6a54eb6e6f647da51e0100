5348412730392b21a5a8d35c372e244c1c673532
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-14 15:02:34 -0400

    Bonnie Doon 1.1  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -2,7 +2,7 @@
     <div>
         <a href="https://www.instagram.com/shopbdc_" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
         <a href="https://www.facebook.com/bonniedoonsc" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
-        <a href="https://www.pinterest.ca/bonniedoonsc" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+        <a href="https://www.pinterest.ca/bonniedoonsc" target="_blank"><i class="fa fa-snpachat-ghost" aria-hidden="true"></i></a>
         <a href="https://www.twitter.com/bonniedoonsc" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
     </div>
     
