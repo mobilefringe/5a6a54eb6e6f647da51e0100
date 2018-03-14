77a6d76983035f48b7f0af100969d692bd97686e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-14 15:05:41 -0400

    Bonnie Doon 1.1  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -2,7 +2,7 @@
     <div>
         <a href="https://www.instagram.com/shopbdc_" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
         <a href="https://www.facebook.com/bonniedoonsc" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
-        <a href="//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/jpeg/1521054167714/28907698_10156615165667323_2001433750_n.jpg" data-lightbox="Snapchat" data-title="Bonnie Doon Snapchat"><i class="fa fa-snpachat-ghost" aria-hidden="true"></i></a>
+        <a href="//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/jpeg/1521054167714/28907698_10156615165667323_2001433750_n.jpg" data-lightbox="Snapchat" data-title="Bonnie Doon Snapchat"><i class="fa fa-snapchat-ghost" aria-hidden="true"></i></a>
         <a href="https://www.twitter.com/bonniedoonsc" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
     </div>
     
