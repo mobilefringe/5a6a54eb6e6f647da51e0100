69892ec1e510f6a13ad010a490fb40c21e22c2bd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-19 11:20:42 -0400

    Bonnie Doon 1.1  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -11,7 +11,7 @@
 		<div class="site_container page_content">
 			<div id="events_container" v-if="promotions.length > 0">
 				<paginate name="promos" v-if="promos" :list="promos" class="paginate-list margin-60" :per="4">
-					<div class="row event_container" v-for="(promo,index) in paginated('promos')" :class="{ 'last': index === (paginated('promos').length - 1) }">
+					<div class="row event_container" v-for="(promo,index) in paginated('promos')" :class="{ 'last': index === (paginated('promos').length - 1) }" :key="promo.id">
 						<div class="col-sm-6 col-md-4 event_image_container">
 							<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more"  v-lazy-container="{ selector: 'img' }">
 								<img :data-src="promo.store.store_front_url_abs"  data-loading='//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/png/1521035009104/Screen Shot 2018-03-14 at 9.43.24 AM.png' class="event_image image" alt=""/>
