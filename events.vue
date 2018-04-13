e3c9621ed079203afb43c3d90b49fb6b74141d69
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-13 12:02:12 -0400

    Bonnie Doon 1.1  /events.vue

--- events.vue
+++ events.vue
@@ -179,8 +179,8 @@
                                 value.store = {};
                                 value.store.store_front_url_abs =  "//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/png/1518554684072/bonniedoonlogo.png";
                             }
-                            if (value.image_url != null && value.store != undefined && _.includes(value.image_url, 'missing')) {
-                                value.store.store_front_url_abs = "//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/png/1518554684072/bonniedoonlogo.png";
+                            if (value.image_url != null && value.image_url != undefined && _.includes(value.image_url, 'missing')) {
+                                value.value.image_url = "//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/png/1518554684072/bonniedoonlogo.png";
                             }
                             temp_promo.push(value);
                         }
