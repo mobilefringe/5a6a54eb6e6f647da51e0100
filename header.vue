2ba96de9d2ebca489e98ef008687252a5599f794
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 18:06:59 -0400

    Bonnie Doon 1.2  /header.vue

--- header.vue
+++ header.vue
@@ -83,3 +83,54 @@
 <!--        				</div>-->
 <!--    				</div>-->
 <!--				</div>-->
+
+<script>
+    define(["Vue", "vuex", 'vue!social_links.vue', "bootstrap-vue", 'json!menu_items.json'], function (Vue, Vuex, SocialLinks, BootstrapVue, MenuItems) {
+        Vue.use(BootstrapVue);
+        return Vue.component("header-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    dataLoaded: false,
+                    show_mobile_menu: false,
+                    property_logo: "//codecloud.cdn.speedyrails.net/sites/5aec62a16e6f64422c090000/image/png/1520457420000/whitelogo1@2x.png",
+                    menu_items: MenuItems,
+                }
+            },
+            watch: {
+                $route: function() {
+                    // hide dropdown when route changes
+                    _.forEach(this.menu_items, function(value, key) {
+                        value.open_dropdown = false;
+                    });
+                    this.show_mobile_menu = false; //close menu when navigating to new page
+                },
+                show_mobile_menu: function() {
+                    if(this.show_mobile_menu === true){
+                        document.body.classList.add("no-scroll");
+                    } else if (this.show_mobile_menu === false) {
+                        document.body.classList.remove("no-scroll");
+                    }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'getTodayHours'
+                ]),
+                todays_hours() {
+                    return this.getTodayHours;
+                }
+            },
+            methods: {
+                toggleDropdown(index) {
+                    console.log("menu is", this.menu_items[index]);
+                     this.menu_items[index].open_dropdown = true;
+                }
+                
+            }
+            
+        });
+    });
+</script>
