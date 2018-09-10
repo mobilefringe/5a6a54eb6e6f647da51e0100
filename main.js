3d012e16c1c032c3dcc7217035f5ecf7bada77cc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-10 14:31:20 -0400

    Bonnie Doon 1.1  /main.js

--- main.js
+++ main.js
@@ -136,7 +136,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                             },
                             {
                                 name: "header.leasing",
-                                href: "/contact_us"
+                                href: "pages/bonniedoon-leasing"
                             },
                             {
                                 name: "header.hours",
