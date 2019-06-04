65c0133281ee8f485ccce0d797d8631f22aae255
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-06-04 10:49:59 -0400

    Bonnie Doon 1.1  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -6,7 +6,7 @@
         <a href="https://www.facebook.com/bonniedoonsc" target="_blank">
             <i class="fa fa-facebook-official" aria-hidden="true"></i>
         </a>
-        <a href="//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/jpeg/1521054167714/28907698_10156615165667323_2001433750_n.jpg" data-lightbox="Snapchat" data-title="BonnieDoon Snapchat">
+        <a href="//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/jpeg/1521054167714/28907698_10156615165667323_2001433750_n.jpg" data-lightbox="Snapchat" data-title="Bonnie Doon Snapchat">
             <i class="fa fa-snapchat-ghost"></i>
         </a>
         <a href="https://www.twitter.com/bonniedoonsc" target="_blank">
