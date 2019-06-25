d4e0b3f34124196a90827dcc3a9847b4aab1ac8d
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2019-06-25 16:42:40 -0400

    Bonnie Doon 1.1  /main.js

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                             },
                             {
                                 name: "header.download_directory",
-                                href: "//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/application/pdf/1548435641000/BD Directory Feb 2019.pdf",
+                                href: "//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/application/pdf/1561495347000/Bonnie Doon Directory 06-20-2019_PRINT.pdf",
                                 external: true
                             }
                         ]
