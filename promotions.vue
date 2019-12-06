8cae364e250271e96ec7c2b2f5d97c3b22da4f17
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-12-06 14:21:29 -0500

    Bonnie Doon 1.1  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -11,7 +11,7 @@
 		<div class="site_container page_content">
 			<div id="events_container" v-if="promotions.length > 0">
 			
-					<div class="row event_container" v-for="(promo,index) in promos" :class="{ 'last': index === promos.length - 1) }" :key="promo.id">
+					<div class="row event_container" v-for="(promo,index) in promos" :class="{ 'last': index === promos.length - 1 }" :key="promo.id">
 						<div class="col-sm-6 col-md-4 event_image_container">
 							<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more"  v-lazy-container="{ selector: 'img' }">
 								<img :data-src="promo.image_url"  data-loading='//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/png/1521035009104/Screen Shot 2018-03-14 at 9.43.24 AM.png' class="event_image image" alt=""/>
