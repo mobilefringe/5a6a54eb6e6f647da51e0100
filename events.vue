696e300574842c1cef6aa7aaba51d105eaa779d4
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-12-11 12:15:05 -0500

    Bonnie Doon 1.1  /events.vue

--- events.vue
+++ events.vue
@@ -182,7 +182,8 @@
                             temp_promo.push(value);
                         }
                     });
-                    temp_promo = _.sortBy(temp_promo, [function(o) { return o.end_date; }]);
+                    // temp_promo = _.sortBy(temp_promo, [function(o) { return o.end_date; }]);
+                    _.sortBy(temp_promo, [function(o) { return o.end_date; }]);
                     return temp_promo;
                 },
             },
