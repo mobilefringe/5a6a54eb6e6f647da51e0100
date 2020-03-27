955b22deaa244b78f98c61d02d3986dc35e06bac
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-27 17:05:16 -0400

    Bonnie Doon 1.1  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -2,7 +2,7 @@
     <div v-if="currentPage">
         <div v-if="pageBanner && currentPage.slug == 'bonniedoon-covid-19-update'" class="page_header" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/png/1585342622000/COVID-19.png)' }">
 		</div>
-        <div v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+        <div v-else-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
 			<!--http://via.placeholder.com/1920x300-->
 			<div class="site_container">
 				<div class="header_content">
