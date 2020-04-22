b3ebef6944207203da3f5931e57a634b809bcdbc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2020-04-22 14:23:25 -0400

    Bonnie Doon 1.1  /find_us.vue

--- find_us.vue
+++ find_us.vue
@@ -3,7 +3,7 @@
 	    <div  v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')', marginBottom:'0px'}">
 			<div class="site_container">
 				<div class="header_content">
-					<h1>Find Us</h1>
+					<h1><span>Find Us</span></h1>
 				</div>
 			</div>
 		</div>  
