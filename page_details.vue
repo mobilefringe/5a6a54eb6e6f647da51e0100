9fced485e2dfc52e1a8894ba9f719bc2b8bc429b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2020-04-22 14:25:33 -0400

    Bonnie Doon 1.1  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -4,24 +4,24 @@
 			http://via.placeholder.com/1920x300
 			<div class="site_container">
 				<div class="header_content">
-					<h1 v-if="locale=='en-ca'">{{currentPage.title}}</h1>
-					<h1 v-else>{{currentPage.title_2}}</h1>
-				</div>
-			</div>
-		</div>
-		
-		
-		<div v-if="pageBanner && currentPage.slug == 'bonniedoon-covid-19-update'" class="page_header" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/png/1585843121000/BannerCOVID-19.png)' }">
-		</div>
-        <div v-else-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
-			<!--http://via.placeholder.com/1920x300-->
-			<div class="site_container">
-				<div class="header_content">
 					<h1 v-if="locale=='en-ca'"><span>{{currentPage.title}}</span></h1>
 					<h1 v-else><span>{{currentPage.title_2}}</span></h1>
 				</div>
 			</div>
 		</div>
+		
+		
+		<!--<div v-if="pageBanner && currentPage.slug == 'bonniedoon-covid-19-update'" class="page_header" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5a8c43eb6e6f641a29020000/image/png/1585843121000/BannerCOVID-19.png)' }">-->
+		<!--</div>-->
+  <!--      <div v-else-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">-->
+			<!--http://via.placeholder.com/1920x300-->
+		<!--	<div class="site_container">-->
+		<!--		<div class="header_content">-->
+		<!--			<h1 v-if="locale=='en-ca'"><span>{{currentPage.title}}</span></h1>-->
+		<!--			<h1 v-else><span>{{currentPage.title_2}}</span></h1>-->
+		<!--		</div>-->
+		<!--	</div>-->
+		<!--</div>-->
 		
 		
 		<div class="site_container inside_page_content">
