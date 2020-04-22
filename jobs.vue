5de4dd8664cc713bd31615af77d6c2c432cffab5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2020-04-22 14:18:17 -0400

    Bonnie Doon 1.1  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -3,7 +3,7 @@
 		<div class="page_header" v-if="promoBanner" v-bind:style="{ backgroundImage: 'url(' + promoBanner.image_url + ')' }">
 			<div class="site_container">
 				<div class="header_content cap">
-					<h1>{{$t("jobs_page.jobs")}}</h1>
+					<h1><span>{{$t("jobs_page.jobs")}}</span></h1>
 				</div>
 			</div>
 		</div>
